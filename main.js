5142bb0be7b79e2f00a71797f9e54e8c2d72f311
WebpageCoder53 <133002197+WebpageCoder53@users.noreply.github.com>
2024-01-31 17:53:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+
+
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -12,7 +19,7 @@ function setup() {
 function draw() {
     background("#28f6b9");
     fill("#e043af");
-    textSize(69);
+    textSize(difference / 3);
     textFont("Times New Roman");
     text("Priyanshu", 100, 205);
 }
@@ -24,5 +31,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
